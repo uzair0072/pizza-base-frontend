a0a30d5965c2390e2a03639563f87c49f7c451c2
Muhammad Uzair <muhammad.uzair0072@gmail.com>
2023-06-08 15:39:23 +0500

    slides carrousel added

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,43 +1,65 @@
 import React, { useState, useEffect } from 'react';
-// import slide1 from "..//..//assets/images/slide1.svg";
-// import slide2 from "..//..//assets/images/slide2.png";
-// import slide3 from "..//..//assets/images/slide3.png";
+import "./slider.css";
 
-
-const HomepageSlider = () => {
+const SlideCarousel = ({ slides }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const slideImages = ['./slide1.svg', './slide2.png', './slide3.png'];
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % slideImages.length);
-    }, 3500);
-    return () => clearInterval(interval);
-  }, []);
-
-  const prevSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide - 1 + slideImages.length) % slideImages.length);
-  };
+  const [isHovered, setIsHovered] = useState(false);
 
   const nextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide + 1) % slideImages.length);
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
   };
 
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide - 1 + slides.length) % slides.length);
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  useEffect(() => {
+    let timer = null;
+    if (!isHovered) {
+      timer = setInterval(() => {
+        setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+      }, 3500);
+    }
+
+    return () => clearInterval(timer);
+  }, [isHovered, slides.length]);
+
   return (
-    <div className="slider">
-      <div className="slides">
-        {slideImages.map((image, index) => (
+    <div className="slide-carousel" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="slides" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
+        {slides.map((slide, index) => (
           <div key={index} className={`slide ${index === currentSlide ? 'active' : ''}`}>
-            <img src={image} alt={`Slide ${index + 1}`} />
+            {slide}
           </div>
         ))}
       </div>
-      <div className="scroll-arrows">
-        <div className="scroll-arrow left-arrow" onClick={prevSlide}></div>
-        <div className="scroll-arrow right-arrow" onClick={nextSlide}></div>
+      <div className="carousel-navigation">
+        <button className="prev-button" onClick={prevSlide}>
+          &lt;
+        </button>
+        <button className="next-button" onClick={nextSlide}>
+          &gt;
+        </button>
+      </div>
+      <div className="slide-dots">
+        {slides.map((_, index) => (
+          <div
+            key={index}
+            className={`slide-dot ${index === currentSlide ? 'active' : ''}`}
+            onClick={() => setCurrentSlide(index)}
+          />
+        ))}
       </div>
     </div>
   );
 };
 
-export default HomepageSlider;
+export default SlideCarousel;
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,12 +1,21 @@
 import React from "react";
 import NavBar from "../../components/navbar/navbar";
-import HomepageSlider from "../../components/slider/slider";
+import SlideCarousel from "../../components/slider/slider";
+import slide1 from '..//..//assets/images/slide1.svg';
+import slide2 from '..//..//assets/images/slide2.png';
+import slide3 from '..//..//assets/images/slide3.png';
+import "./homepage.css";
 
 function HomePage() {
+    const slides = [
+        <img src={slide1} alt="Slide 1" />,
+        <img src={slide2} alt="Slide 2" />,
+        <img src={slide3} alt="Slide 3" />,
+    ];
     return (
         <>
-        <NavBar />
-        <HomepageSlider />
+            <NavBar />
+            <SlideCarousel slides={slides} />
         </>
     )
 }
