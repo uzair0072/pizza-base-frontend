ed571b8d2c06854fdcc4f4ad5eb9eb021753b77e
Muhammad Uzair <muhammad.uzair0072@gmail.com>
2023-06-09 11:56:02 +0500

    logo  carousel updated

--- src/components/logo-slider/logo-slider.js
+++ src/components/logo-slider/logo-slider.js
@@ -1,54 +1,85 @@
 import React, { useRef } from 'react';
-import { useSnapCarousel } from 'react-snap-carousel';
 import "./logo-slider.css";
+import pizzaCategory from "..//..//assets/images/pizza-category.svg";
+import burgerCategory from "..//..//assets/images/burger-category.svg";
+import platterCategory from "..//..//assets/images/platter-category.svg";
+import pastasCategory from "..//..//assets/images/pastas-category.svg";
+import sandwichesCategory from "..//..//assets/images/sandwiches-category.svg";
+import sideOrderCategory from "..//..//assets/images/side-order-category.svg";
+import addOnsCategory from "..//..//assets/images/add-ons-category.svg";
 
 const LogoCarousel = () => {
-    const scrollRef = useRef(null);
-    const { scrollToNextItem, scrollToPrevItem } = useSnapCarousel(scrollRef);
+  const carouselRef = useRef(null);
 
-    return (
-        <>
-            <div>
-                <h3 style={{ marginLeft: '50px', marginTop: '50px' }}> Main Categories </h3>
-            </div>
-            <div style={{ display: 'flex', alignItems: 'center', margin: '30px 50px', overflow: 'hidden' }}>
-                <button onClick={scrollToPrevItem}>&lt;</button>
-                <ul
-                    ref={scrollRef}
-                    style={{
-                        display: 'flex',
-                        margin: '0',
-                        padding: 0,
-                        gap: '12px',
-                        listStyle: 'none',
-                        scrollBehavior: 'smooth',
-                        scrollSnapType: 'x mandatory',
-                        overflowX: 'scroll',
-                        flexWrap: 'nowrap',
-                    }}
-                >
-                    {Array.from({ length: 12 }).map((_, i) => (
-                        <li
-                            style={{
-                                background: '#FFED4F',
-                                borderRadius: '14px',
-                                width: '160px',
-                                height: '220px',
-                                flexShrink: 0,
-                                color: '#000',
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center'
-                            }}
-                        >
-                            Item {i}
-                        </li>
-                    ))}
-                </ul>
-                <button onClick={scrollToNextItem}>&gt;</button>
-            </div>
-        </>
-    );
+  const handlePrevious = () => {
+    carouselRef.current.scrollLeft -= carouselRef.current.offsetWidth;
+  };
+
+  const handleNext = () => {
+    carouselRef.current.scrollLeft += carouselRef.current.offsetWidth;
+  };
+
+  const logos = [
+    { imageUrl: pizzaCategory, text: "Pizza Deals" },
+    { imageUrl: burgerCategory, text: "Burger Deals" },
+    { imageUrl: platterCategory, text: "Platters" },
+    { imageUrl: sandwichesCategory, text: "Sandwiches" },
+    { imageUrl: pastasCategory, text: "Pastas" },
+    { imageUrl: sideOrderCategory, text: "Side Order" },
+    { imageUrl: addOnsCategory, text: "Add-Ons" },
+    { imageUrl: "https://www.google.com.pk/", text: "Other" },
+    { imageUrl: "https://www.google.com.pk/", text: "Other" },
+  ];
+
+  return (
+    <>
+      <div>
+        <h3 style={{ marginLeft: '50px', marginTop: '50px' }}> Main Categories </h3>
+      </div>
+      <div className="carousel-container" style={{ display: 'flex', margin: '30px 50px', position: 'relative' }}>
+        <ul
+          ref={carouselRef}
+          style={{
+            display: 'flex',
+            margin: 0,
+            padding: 0,
+            gap: '12px',
+            listStyle: 'none',
+            overflowX: 'auto',
+            scrollSnapType: 'x mandatory',
+            scrollBehavior: 'smooth',
+          }}
+        >
+          {logos.map((logo, index) => (
+            <li
+              key={index}
+              style={{
+                background: '#FFED4F',
+                borderRadius: '14px',
+                width: '160px',
+                height: '220px',
+                flexShrink: 0,
+                color: '#000',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+              }}
+            >
+              <img src={logo.imageUrl} alt={logo.text} style={{ width: '100px', height: '100px', marginBottom: '10px' }} />
+              <span style={{fontWeight: 'bold'}}>{logo.text}</span>
+            </li>
+          ))}
+        </ul>
+        <button className="carousel-button prev-button" onClick={handlePrevious}>
+          &lt;
+        </button>
+        <button className="carousel-button next-button" onClick={handleNext}>
+          &gt;
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default LogoCarousel;
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -18,6 +18,7 @@ function HomePage() {
             <NavBar />
             <ImageCarousel slides={slides} />
             <LogoCarousel />
+            <hr></hr>
         </>
     )
 }
